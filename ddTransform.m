3aee7b976f07a96e203ccac599a0255fa19b1a8c
donnelr3 <43070609+donnelr3@users.noreply.github.com>
2018-12-31 17:13:07 +0000

    Update ddTransform.m

--- ddTransform.m
+++ ddTransform.m
@@ -1,6 +1,6 @@
 function transField = ddTransform(R,reproField,totalField,K)
 % Function to read in a matrix where each entry represents number of aphids
-% on corresponing plant in spatial array
+% on corresponing plant in spatial array. Function transforms matrix to density dependent reproduction rates per plant.
 Kadj=(1-(K<0))*K;
 Kadj2=Kadj+(Kadj==0)*0.000001;
 transField=R*reproField.*(1-(totalField/Kadj2));
